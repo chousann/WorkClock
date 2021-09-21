328ef5e651b51446884a502e749046613e776443
chousann <1769862201@qq.com>
2021-09-21 12:57:12 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,59 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MatTableModule, MatCardModule, MatExpansionModule, MatBadgeModule, MatSidenavModule, MatTabsModule, MatSnackBarModule, MatDividerModule, MatToolbarModule, MatIconModule, MatButtonModule, MatCheckboxModule, MatInputModule, MatListModule, MatDialogModule, MatProgressSpinnerModule, MatProgressBarModule, MatMenuModule, MatRadioModule, MatDatepickerModule, MatNativeDateModule} from '@angular/material';
+import { DetailComponent } from './detail/detail.component';
+import { DakaComponent } from './daka/daka.component';
+import { LocalstorageService } from './service/localstorage.service';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+const appRoutes: Routes = [
+  {
+    path: '', component: DakaComponent
+  },
+  {
+    path: 'detail', component: DetailComponent
+  }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailComponent,
+    DakaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatCardModule,
+    MatListModule,
+    MatDialogModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatSnackBarModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatBadgeModule,
+    MatExpansionModule,
+    MatRadioModule,
+    MatTableModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } // <-- debugging purposes only
+    ),
   ],
-  providers: [],
+  providers: [LocalstorageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
