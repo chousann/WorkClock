c95ba74b51a9633dc803925284a1fd352c796d29
chousann <45139336+chousann@users.noreply.github.com>
2023-06-15 20:23:26 +0800

    Create alpine.sh

---
+++ alpine.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# 安装依赖
+apk update && apk add --no-cache wget unzip
+# 下载 V2Ray
+wget https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -O /tmp/v2ray.zip
+# 解压 V2Ray
+mkdir -p /usr/local/share/v2ray
+unzip /tmp/v2ray.zip -d /usr/local/share/v2ray
+# 配置 V2Ray
+cat << EOF > /usr/local/share/v2ray/config.json
+{
+  "inbounds": [
+    {
+      "port": 12345, // 修改监听端口号
+      "protocol": "vmess",
+      "settings": {
+        "clients": [
+          {
+            "id": "00000000-0000-0000-0000-000000000000", // 修改 UUID
+            "alterId": 64 // 修改 alterId
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "tcp" // 修改传输协议
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "settings": {}
+    }
+  ]
+}
+EOF
+# 启动 V2Ray
+/usr/local/share/v2ray/v2ray -config /usr/local/share/v2ray/config.json
